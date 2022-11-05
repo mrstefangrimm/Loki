244b8c037a161e5af06bddbc7975213465ee26ca
mrstefangrimm <motionssystems@grimm.com>
2022-11-05 11:33:06 +0100

    Update make.msvc.bat to work with Visual Studio 2022.

--- make.msvc.bat
+++ make.msvc.bat
@@ -4,52 +4,24 @@ if defined LOKI_TMP (
 	goto STARTCOMPILING
 )
 
-:: Viual C++ 7.1
-if defined VS71COMNTOOLS  (
-	if exist "%VS71COMNTOOLS%\vsvars32.bat" (
-		echo -
-		echo - Visual C++ 7.1 found.
-		echo -
-		call "%VS71COMNTOOLS%\vsvars32.bat"
-		set LOKI_TMP=true
-		goto STARTCOMPILING
-	)
-)
+:: Viual C++ 2022 (17)
 
-:: Viual C++ 8.0
-if defined VS80COMNTOOLS (
-	if exist "%VS80COMNTOOLS%\vsvars32.bat" (
-		echo -
-		echo - Visual C++ 8.0 found.
-		echo -
-		call "%VS80COMNTOOLS%\vsvars32.bat"
-		set LOKI_TMP=true
-		goto STARTCOMPILING
-	)
-)
-
-:: Toolkit 2003
-if defined VCToolkitInstallDir (
-	if exist "%VCToolkitInstallDir%\vcvars32.bat" (
-		echo -
-		echo - VC 7.1 Toolkit found.
-		echo -
-		call "%VCToolkitInstallDir%\vcvars32.bat"
-		set LOKI_TMP=true
-		set LOKI_MSVC_NOLIB=true
-		goto STARTCOMPILING
-	)
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\vsdevcmd\ext\vcvars.bat" (
+	echo -
+	echo - Visual C++ 2022 found.
+	echo -
+	call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\vsdevcmd\ext\vcvars.bat"
+	set LOKI_TMP=true
+	goto STARTCOMPILING
 )
 
 
 echo -
-echo - No Visual C++ found, please set the enviroment variable 
+echo - No Visual C++ 2022 found, please update this batch file. 
 echo - 
-echo - VCToolkitInstallDir  or  VS71COMNTOOLS or VS80COMNTOOLS 
+echo - expected path for vcvars.bat is:
 echo - 
-echo - to your Visual Studio folder which contains vsvars32.bat.
-echo - 
-echo - Or call the vsvars32.bat.
+echo - C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\vsdevcmd\ext
 echo -
 
 goto ERROR
